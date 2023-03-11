b6364bed65fa878ffc43f0c43e48d4af4636ad16
MuadDAtr <int.stron@protonmail.com>
2023-03-11 23:37:20 +0100

    adding videos id

--- smpl.py
+++ smpl.py
@@ -9,17 +9,18 @@ api = Api(app)
 videos = {
     'vid_1': {'title': "Harry Potter"},
     'vid_2': {'title': "The Lord of The Rings"},
-    'vid_3': {'title': "The Godfather"}
+    'vid_3': {'title': "The Godfather"},
+    'vid_4': {'title': "The Lion King"}
 
 }
 
 class Video(Resource):
-    def get (self):
-        return videos
+    def get (self, video_id):
+        return videos[video_id]
 
 
 
-api.add_resource(Video, '/')
+api.add_resource(Video, '/videos/<video_id>')
 
 if __name__ == '__main__':
     app.run()
