dcb1cfac135f493d1ab06581f63e5fd0a37fcc09
MuadDAtr <int.stron@protonmail.com>
2023-03-14 22:55:01 +0100

    refacturing

--- smpl.py
+++ smpl.py
@@ -20,6 +20,8 @@ class Video(Resource):
     def get (self, video_id):
         if video_id == "all":
             return videos
+        if video_id not in videos:
+            abort(404, message= f"Video {video_id} not found") 
         return videos[video_id]
     
     def put(self, video_id):
